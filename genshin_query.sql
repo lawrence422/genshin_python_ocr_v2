57997c907696c46ee08bbc760df61a292abc153c
lawrence422 <lawrence422@gmail.com>
2023-02-15 07:30:41 +0800

    completed

---
+++ genshin_query.sql
@@ -0,0 +1,15 @@
+SELECT * FROM genshin_artifact.artifact limit 1000,1000;
+
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='深林' AND `artifact_type`='头' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='深林' AND `artifact_type`='羽毛' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='绝缘' AND `artifact_type`='羽毛' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='绝缘' AND `artifact_type`='头' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='追忆' AND `artifact_type`='羽毛' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='冰套' AND `artifact_type`='羽毛' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='饰金' AND `artifact_type`='羽毛' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='饰金' AND `artifact_type`='头' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='乐团' AND `artifact_type`='花' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='魔女' AND `artifact_type`='羽毛' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='魔女' AND `artifact_type`='头' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='魔女' AND `artifact_type`='花' ;
+SELECT * FROM genshin_artifact.`artifact` WHERE `artifact_set`='角斗' AND `artifact_type`='羽毛' ;
